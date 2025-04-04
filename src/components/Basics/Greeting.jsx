159e09f0eb301db383e4d93acec8cd8bc9e8ba92
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2025-04-04 23:17:39 +0530

    feature(text-responsive): for large screen text size added

--- src/components/Basics/Greeting.jsx
+++ src/components/Basics/Greeting.jsx
@@ -1,7 +1,7 @@
 const Greeting = () => {
   return (
     <div className="h-screen w-full bg-slate-950 text-white flex items-center justify-center">
-      <h1 classname="text-4xl md:text-6xl">REACTLAB</h1>
+      <h1 classname="text-4xl md:text-6xl lg:text-9xl">REACTLAB</h1>
     </div>
   );
 };
