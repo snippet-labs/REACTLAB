05b17118edcd1d3f2cae0741251955898737eabb
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2025-04-04 23:14:45 +0530

    feature(text-responsive): default text size added

--- src/components/Basics/Greeting.jsx
+++ src/components/Basics/Greeting.jsx
@@ -1,7 +1,7 @@
 const Greeting = () => {
   return (
     <div className="h-screen w-full bg-slate-950 text-white flex items-center justify-center">
-      <h1>REACTLAB</h1>
+      <h1 classname="text-4xl">REACTLAB</h1>
     </div>
   );
 };
