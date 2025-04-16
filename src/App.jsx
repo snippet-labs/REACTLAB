e56651731ca458fdd68c04cab0349595e87c4a29
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2025-04-17 00:29:55 +0530

    hook(useRefHook): completed the useRef form project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+// COMPONENTS
 import Greeting from "./components/Basics/Greeting";
 // import CounterEffect from "./components/Hooks/useEffectHook/Counter1";
 import Counter from "./components/Hooks/useStateHook/Counter";
 import Form from "./project/Form";
 import ThemeToggler from "./project/ThemeToggler";
+import HandlingForm from "./components/Hooks/useRefHook/HandlingForm";
 
 // TRY IT YOURSELF COMPONENTS
 // import ModifiedCounter from "./tryityourself/tushit/ModifiedCounter";
 
 const App = () => {
-  return <Form/>;
+  return <HandlingForm />;
 };
 
 export default App;
