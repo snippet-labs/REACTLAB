29f883804de8cac7854fb94d26f8dfe1da8e5a8b
Hemangshu-Dey <hemangshu1904@gmail.com>
2025-04-24 11:52:50 +0530

    useCallback&useMemo(examples): completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // COMPONENTS
 import Greeting from "./components/Basics/Greeting";
-import Counter from "./components/Hooks/useStateHook/Counter";
+// import Counter from "./components/Hooks/useStateHook/Counter";
 import Form from "./project/Form";
 import ThemeToggler from "./project/ThemeToggler";
 import HandlingForm from "./components/Hooks/useRefHook/HandlingForm";
+import ChangeTheme from "./components/Hooks/useMemo/ChangeTheme";
+import DisplayList from "./components/Hooks/useCallback/DisplayList";
 // TRY IT YOURSELF COMPONENTS
 import FormDetails from "./tryityourself/Akash/FormDetails";
 import RandomBackground from "./tryityourself/Akash/RandomBackground";
 
 const App = () => {
-  return <RandomBackground />;
+  return <DisplayList />;
 };
 
 export default App;
